7dc15a3023b23fea63f6fe34a277cb780d0c7d96
luanwillianzh <74968256+luanwillianzh@users.noreply.github.com>
2025-01-13 10:55:25 -0300

    Update index.py

--- src/index.py
+++ src/index.py
@@ -32,7 +32,7 @@ def novel_info(novel_id):
     title = t.translate(html.fromstring(r.text).xpath("//h3[@class='title']/text()")[0]).text #.text.find_all("h3", {"class": "title"})[0].text, dest="pt").text
     desc = t.translate(h.find_all("div", {"class": "desc-text"})[0].text, dest="pt").text
     caps = {}
-    lista = [ caps.update({a.values()[0]:f"http://104.18.37.248/b/{novel_id}/{a.values()[0]}"}) for a in html.fromstring(requests.get(f"https://novelbin.com/ajax/chapter-option?novelId={novel_id}").text).xpath("//option") ]
+    lista = [ caps.update({a.values()[0]:{a.values()[1]}) for a in html.fromstring(requests.get(f"https://novelbin.com/ajax/chapter-option?novelId={novel_id}").text).xpath("//option") ]
     return {"sucesso": True, "resultado": {"title": title, "desc": desc, "cover": f"https://novelbin.me/media/novel/{novel_id}.jpg", "chapters": caps}}
 
 @app.get("/novel/{novel_id}/{chapter_id}")
